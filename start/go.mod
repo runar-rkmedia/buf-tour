0eb4973231efbf190bd0eb77efb3f53a1046b115
Runar Kristoffersen <gh@runar.dev>
2022-08-05 17:49:49 +0200

    add connect-go

--- start/go.mod
+++ start/go.mod
@@ -3,6 +3,8 @@ module github.com/bufbuild/buf-tour/petstore
 go 1.19
 
 require (
+	github.com/bufbuild/connect-go v0.3.0
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
 	google.golang.org/genproto v0.0.0-20220805133916-01dd62135a58
 	google.golang.org/grpc v1.48.0
 	google.golang.org/protobuf v1.28.1
@@ -10,7 +12,6 @@ require (
 
 require (
 	github.com/golang/protobuf v1.5.2 // indirect
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.5 // indirect
 )
--- start/server/main.go
+++ start/server/main.go
@@ -5,16 +5,34 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 
 	// This import path is based on the name declaration in the go.mod,
 	// and the gen/proto/go output location in the buf.gen.yaml.
 	petv1 "github.com/bufbuild/buf-tour/petstore/gen/proto/go/pet/v1"
+	petv1connect "github.com/bufbuild/buf-tour/petstore/gen/proto/go/pet/v1/petv1connect"
+	"github.com/bufbuild/connect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 	"google.golang.org/grpc"
 )
 
 func main() {
-	if err := run(); err != nil {
-		log.Fatal(err)
+	if false {
+
+		if err := run(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		petter := &PetServer{}
+		mux := http.NewServeMux()
+		path, handler := petv1connect.NewPetStoreServiceHandler(petter)
+		mux.Handle(path, handler)
+		fmt.Println("starting server on", path)
+		http.ListenAndServe(
+			"localhost:8080",
+			h2c.NewHandler(mux, &http2.Server{}),
+		)
 	}
 }
 
@@ -38,6 +56,37 @@ func run() error {
 // petStoreServiceServer implements the PetStoreService API.
 type petStoreServiceServer struct {
 	petv1.UnimplementedPetStoreServiceServer
+}
+
+type PetServer struct{}
+
+func (s *PetServer) PutPet(
+	ctx context.Context,
+	req *connect.Request[petv1.PutPetRequest],
+) (*connect.Response[petv1.PutPetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&petv1.PutPetResponse{})
+	res.Header().Set("PetV", "v1")
+	return res, nil
+}
+func (s *PetServer) GetPet(
+	ctx context.Context,
+	req *connect.Request[petv1.GetPetRequest],
+) (*connect.Response[petv1.GetPetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	log.Println("GET, ", req.Msg)
+	res := connect.NewResponse(&petv1.GetPetResponse{})
+	res.Header().Set("PetV", "v1")
+	return res, nil
+}
+func (s *PetServer) DeletePet(
+	ctx context.Context,
+	req *connect.Request[petv1.DeletePetRequest],
+) (*connect.Response[petv1.DeletePetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&petv1.DeletePetResponse{})
+	res.Header().Set("PetV", "v1")
+	return res, nil
 }
 
 // PutPet adds the pet associated with the given request into the PetStore.
